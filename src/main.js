03122b50e3c6dc926572ee2c422070a2e7f7fc2e
Dustin Byers <dbyers84@gmail.com>
2018-02-14 07:48:00 -0800

    Revert "Dustin"

--- src/main.js
+++ src/main.js
@@ -1,14 +1,9 @@
-// import React application reqs
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 
-// import p5 library
 import P5Wrapper from 'react-p5-wrapper';
+import sketch from './component/app';
 
-// import pong file
-import pong from '../pong';
-
-// define base Appm component
 class App extends Component {
   state = { data: '' };
 
@@ -16,12 +11,15 @@ class App extends Component {
     new Promise(resolve => {
       this.setState(state, resolve);
     });
-  // render for main app component, invoke React wrapper with sketch prop including pong component
-  render = () => (
-    <div style={{ width: '100%', border: '1px solid black' }}>
-      <P5Wrapper sketch={pong} />
-    </div>
-  );
+
+  render = () => {
+    console.log('is rendering');
+    return (
+      <div style={{ width: '100%', border: '1px solid black' }}>
+        <P5Wrapper sketch={sketch} />
+      </div>
+    );
+  };
 }
 
 ReactDom.render(<App />, document.getElementById('root'));
