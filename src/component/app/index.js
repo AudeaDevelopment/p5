7796086b6a0ed98b3fc9fa019a623fd019896b31
Joshua Evans <skyfriends1@gmail.com>
2018-02-13 19:11:07 -0800

    fixed main sketch

--- src/component/app/index.js
+++ src/component/app/index.js
@@ -1,9 +1,22 @@
-import React, { PureComponent } from 'react';
+export const sketch = p => {
+  let rotation = 0;
 
-class MainSketch extends PureComponent {
-  render() {
-    return <div />;
-  }
-}
+  p.setup = () => {
+    p.createCanvas(600, 400, p.WEBGL);
+  };
 
-export default MainSketch;
+  p.redraw = props => {
+    if (props.rotation) {
+      rotation = props.rotation * (Math.PI / 180);
+    }
+  };
+
+  p.draw = () => {
+    p.background(100);
+    p.noStroke();
+    p.push();
+    p.rotateY(rotation);
+    p.box(100);
+    p.pop();
+  };
+};
