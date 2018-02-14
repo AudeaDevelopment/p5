b2cc18ed2e826c6d122293ec0d24f2e28fbcd664
Joshua Evans <skyfriends1@gmail.com>
2018-02-13 19:08:55 -0800

    FIXED INITIAL FOR WATCH

--- index.js
+++ index.js
@@ -4,5 +4,4 @@ express()
   .use(express.static(`${__dirname}/dist`))
   .get('*', (req, res) => res.sendFile(`${__dirname}/dist/index.html`))
   .listen(process.env.PORT, () =>
-    console.log('__SERVER_RUNNING__', process.env.PORT),
-  );
+    console.log('__SERVER_RUNNING__', process.env.PORT));
